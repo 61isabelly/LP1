052682fe2be336ef7fdad1f004e2c7b60300f517
Isabelly S <164695350+61isabelly@users.noreply.github.com>
2024-08-16 14:48:26 -0300

    Initial commit of Java exercises

---
+++ src/exercicios/ConversorDias.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ConversorDias {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sua idade em anos: ");
+        int anos = sc.nextInt();
+        System.out.println("Sua idade em meses: ");
+        int meses = sc.nextInt();
+        System.out.println("Sua idade em dias: ");
+        int dias = sc.nextInt();
+
+        int idadeEmDias = (anos * 12 * 30) + (meses * 30) + dias;
+
+        System.out.println("A sua idade total em dias é de: " + idadeEmDias);
+
+        sc.close();
+
+    }
+}
+
+
