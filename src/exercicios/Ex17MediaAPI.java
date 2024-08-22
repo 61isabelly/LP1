87a49a3dbb1f8cb215ff449c81200789853f0a26
Isabelly S <164695350+61isabelly@users.noreply.github.com>
2024-08-22 18:53:49 -0300

    Improving and updating new exercises

--- src/exercicios/Ex17MediaAPI.java
+++ src/exercicios/Ex17MediaAPI.java
@@ -2,7 +2,7 @@ package exercicios;
 
 import java.util.Scanner;
 
-public class ExMediaAPI {
+public class Ex17MediaAPI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite a nota da P1: ");
